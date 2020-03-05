3a8aebde35e88c8de9cd6e5f823520b73e198f12
Faust Fu <faust.fu@gmail.com>
2020-03-05 18:28:15 +0800

    Dev: add samples for db.

---
+++ db01.py
@@ -0,0 +1,24 @@
+# 1. Module:sqlalchemy is for db processing.
+import sqlalchemy as sa
+
+conn = sa.create_engine('sqlite://')
+insert_sql = 'insert into zoo(critter, count, damages) values(?, ?, ?)'
+select_sql = 'select * from zoo'
+
+conn.execute('''
+create table zoo(
+  critter varchar(20) primary key,
+  count int,
+  damages float
+)
+''')
+
+
+conn.execute(insert_sql, 'duck', 10, 0.0)
+conn.execute(insert_sql, 'bear', 2, 1000.0)
+conn.execute(insert_sql, 'tiger', 1, 2000.0)
+
+rows = conn.execute(select_sql)
+
+for row in rows:
+    print(row)
