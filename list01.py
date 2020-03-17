26e43e2d342d892e99212b92f253668cd5600088
Faust Fu <faust.fu@gmail.com>
2020-03-17 15:43:38 +0800

    Dev: add a sample for iteration.

--- list01.py
+++ list01.py
@@ -7,3 +7,9 @@
 # 7. slice statement will copy from the list to make a new list.
 empty_list = []
 print("empty list %s" % empty_list)
+
+some_list = [1, 3, 0, 2]
+for i, x in enumerate(some_list):
+    print(i, x, x * (4 ** i))
+
+print(sum([x * (4 ** i) for i, x in enumerate(some_list)]))
