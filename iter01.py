75d7e8c57f4e2d69493338f2fb1c1ee08c30b5b0
Faust Fu <faust.fu@gmail.com>
2020-09-19 22:28:35 +0800

    Dev: add a case for itertools.

---
+++ iter01.py
@@ -0,0 +1,22 @@
+# 1. There are many utilities for iterator or generator in package:itertools.
+import itertools
+
+MAX_ITERATION = 10
+
+# case 1: infinite loop by a list
+for i, c in enumerate(itertools.cycle('abcd')):
+    print(i, c)
+
+    if i > MAX_ITERATION:
+        break
+
+# case 2: infinite loop by a initial value with fixed interval.
+for i, c in enumerate(itertools.count(5, 2)):
+    print(i, c)
+
+    if i > MAX_ITERATION:
+        break
+
+names = ['Justin', 'Monica', 'Irene', 'Pika', 'caterpillar']
+for length, group in itertools.groupby(names, len):
+    print(length, list(group))
